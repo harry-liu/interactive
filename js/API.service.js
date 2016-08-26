63d8ed2623e72bf859edbbc727f001fcd5a30b20
代码新手 <747311978@qq.com>
2016-08-26 16:00:51 +0800

    fix bugs

--- js/API.service.js
+++ js/API.service.js
@@ -1,8 +1,8 @@
 var APIService = angular.module('APIService', []);
 
 APIService.factory('PublicURL', function(){
-  return 'https://192.168.1.16/api/';
-  //return 'http://hdq.hudongcn.com/api/';
+  return 'http://192.168.1.16/api/';
+  //return 'https://hdq.hudongcn.com/api/';
 })
 
 //save token into localstorage
